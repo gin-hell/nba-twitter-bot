fb85fbfa6fb8d18a4fe0d3c25981ec4c5eaa3416
gin_hell <j.hill0422@gmail.com>
2018-07-06 15:45:20 -0500

    deleted dumb old js file

--- generate.js
+++ generate.js
@@ -29,7 +29,7 @@ var playerName;
 var playerName2;
 var teamName;
 var teamName2;
-var index = 15;
+var index = 16;
 var players = NBA.players;
 var teams = NBA.teams;
 
@@ -94,14 +94,51 @@ function composeTweet(){
 		.then(function(){
 
 
+
+// ~~~~~~~~~~ array of tweet templates
+var tweets = [
+
+			'The ' + teamName + ' and ' + teamName2 + ' have begun talks over a ' + playerName + ' trade.',
+			'The ' + teamName + ' is looking to trade ' + playerName + ' to the ' + teamName2 + ' for a future first round draft pick.',
+			'Sources: ' + playerName + ' has expressed interest in signing with the ' + teamName + '.',
+			'According to league sources, ' + playerName + ' will sign a free agent deal with the ' + teamName + '.',
+			'Free agent ' + playerName + ' is finalizing a one-year deal to return to the ' + teamName + '.',
+			'The ' + teamName + ' will waive ' + playerName + ', per league sources.',
+			'The ' + teamName + ' is reportedly offering ' + playerName + ' to the '+ teamName2 +' in a multi-team trade for ' + playerName2 + '.', 
+			playerName + "'s name continues to surface in trade talks with the " + teamName + ' and the ' + teamName2 + '.',
+			'Proposed multi-team trade could have sent ' + playerName + ' to the ' + teamName + ' for ' + playerName2 + '.',
+			'The ' + teamName + ' waive key assets to make room for ' + playerName + '.',
+			teamName + ' putting pressure on the ' + teamName2 + ' in race for ' + playerName +'.',
+			'The ' + teamName + ' has agreed to trade ' + playerName + ' and a future lottery pick to the ' + teamName2 + ' for two future first round draft picks.',
+			'The ' + teamName + ' announced today that they have traded '+ playerName + ' to the '+ teamName2+' in exchange for cash considerations.',
+			'Sources: Free agent ' + playerName + ' hosts meetings with interested suitors, including the ' + teamName + 'and ' + teamName2 + ' this weekend.',
+			'The ' + teamName + ' has traded ' + playerName + ' to Houston for cash and a 2018 second-round pick, per league sources.',
+			'Sources: ' + teamName + ' working to land both ' + playerName + ' and ' + playerName2 + '.',
+			'The ' + teamName + ' is exercising its $1.5M team option on ' + playerName + ', according to league source.',
+			'Trade Rumors: ' + teamName + ' could look to acquire '+ playerName + ' from ' + teamName2 + '.',
+			playerName + ' has agreed to a 4-yr, $24.5M deal with the ' + teamName + '.',
+			'The ' + teamName + ', having lost ' + playerName + ' in the ' + teamName2 + ' trade, begins talks with ' + playerName2 + ', per sources.',
+			playerName + ' has agreed to a two-way deal with the ' + teamName + ', league sources tell ESPN.',
+			'League sources tell ESPN that ' + playerName + ' has agreed to a one-year deal with the ' + teamName + '.',
+			'Free agent ' + playerName + ' has signed a one-year contract worth $5M to remain with the ' + teamName + '.',
+			'The ' + teamName + ' has traded ' + playerName + ' and a draft consideration to the ' + teamName2 + '.',
+			'League Sources tell ESPN that ' + playerName + ', ' + teamName + ' finalizing a two-year, $12M deal.',
+			'The ' + teamName + ' withdrew qualifying offer on ' + playerName + ', who will become an unrestricted free agent.',
+			'The ' + teamName + ' is re-signing ' + playerName + ', a source told ESPN.',
+			teamName + ' restricted free agent ' + playerName + ' has agreed to a three-year, $30M contract extension.',
+			playerName + ' and the ' + teamName + ' are progressing toward finalizing a four-year, $80M contract extension, league sources tell ESPN.',
+			playerName + ' has committed to sign a deal with the ' + teamName + '.',
+			'Sources on ESPN: ' + teamName + ' player ' + playerName + ' to opt-out, clearing the way to free-agency.',
+			'The ' + teamName + ' has declined the option on ' + playerName + '. He will become a free agent.'
+			]
 // ~~~~~~~~~~ get text from list of tweets:
 		var text = tweets[index];
-		console.log(text);
 		
 		T.post ('statuses/update', {
 			status: text + " #NBATrades #NBAFreeAgency" }, 
 			function(err, data, response) {
-	  			console.log('\n********** new tweet: index ' + index + ' **********');		
+	  			console.log('\n********** new tweet: index ' + index + ' **********');
+				console.log(text);
 			});
 
 		index = (index + 1)% tweets.length;
@@ -173,43 +210,5 @@ setInterval(function(){console.log("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")},
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-// ~~~~~~~~~~ array of tweet templates
-var tweets = [
-
-			'The ' + teamName + ' and ' + teamName2 + ' have begun talks over a ' + playerName + ' trade.',
-			'The ' + teamName + ' is looking to trade ' + playerName + ' to the ' + teamName2 + ' for a future first round draft pick.',
-			'Sources: ' + playerName + ' has expressed interest in signing with the ' + teamName + '.',
-			'According to league sources, ' + playerName + ' will sign a free agent deal with the ' + teamName + '.',
-			'Free agent ' + playerName + ' is finalizing a one-year deal to return to the ' + teamName + '.',
-			'The ' + teamName + ' will waive ' + playerName + ', per league sources.',
-			'The ' + teamName + ' is reportedly offering ' + playerName + ' to the '+ teamName2 +' in a multi-team trade for ' + playerName2 + '.', 
-			playerName + "'s name continues to surface in trade talks with the " + teamName + ' and the ' + teamName2 + '.',
-			'Proposed multi-team trade could have sent ' + playerName + ' to the ' + teamName + ' for ' + playerName2 + '.',
-			'The ' + teamName + ' waive key assets to make room for ' + playerName + '.',
-			teamName + ' putting pressure on the ' + teamName2 + ' in race for ' + playerName +'.',
-			'The ' + teamName + ' has agreed to trade ' + playerName + ' and a future lottery pick to the ' + teamName2 + ' for two future first round draft picks.',
-			'The ' + teamName + ' announced today that they have traded '+ playerName + ' to the '+ teamName2+' in exchange for cash considerations.',
-			'Sources: Free agent ' + playerName + ' hosts meetings with interested suitors, including the ' + teamName + 'and ' + teamName2 + ' this weekend.',
-			'The ' + teamName + ' has traded ' + playerName + ' to Houston for cash and a 2018 second-round pick, per league sources.',
-			'Sources: ' + teamName + ' working to land both ' + playerName + ' and ' + playerName2 + '.',
-			'the ' + teamName + ' is exercising its $1.5M team option on ' + playerName + ', league sources tell @2018_NBA_Trades.',
-			'Trade Rumors: ' + teamName + ' could look to acquire '+ playerName + ' from ' + teamName2 + '.',
-			playerName + ' has agreed to a 4-yr, $24.5M deal with the ' + teamName + '.',
-			'The ' + teamName + ', having lost ' + playerName + ' in the ' + teamName2 + ' trade, begins talks with ' + playerName2 + ', per sources.',
-			playerName + ' has agreed to a two-way deal with the ' + teamName + ', league sources tell ESPN.',
-			'League sources tell ESPN that ' + playerName + ' has agreed to a one-year deal with the ' + teamName + '.',
-			'Free agent ' + playerName + ' has signed a one-year contract worth $5M to remain with the ' + teamName + '.',
-			'The ' + teamName + ' has traded ' + playerName + ' and a draft consideration to the ' + teamName2 + '.',
-			'League Sources tell ESPN that ' + playerName + ', ' + teamName + ' finalizing a two-year, $12M deal.',
-			'The ' + teamName + ' withdrew qualifying offer on ' + playerName + ', who will become an unrestricted free agent.',
-			'The ' + teamName + ' is re-signing ' + playerName + ', a source told ESPN.',
-			teamName + ' restricted free agent ' + playerName + ' has agreed to a three-year, $30M contract extension.',
-			playerName + ' and the ' + teamName + ' are progressing toward finalizing a four-year, $80M contract extension, league sources tell ESPN.',
-			playerName + ' has committed to sign a deal with the ' + teamName + '.',
-			'Sources on ESPN: ' + teamName + ' player ' + playerName + ' to opt-out, clearing the way to free-agency.',
-			'The ' + teamName + ' has declined the option on ' + playerName + '. He will become a free agent.'
-
-			]
 
 
